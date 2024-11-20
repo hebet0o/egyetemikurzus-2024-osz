89c93c7faf4a5be1189a2e9aa6e8f81d55b0e990
hebet0o <gaboriszak46@gmail.com>
2024-11-20 18:58:16 +0100

    project letrehozasa

---
+++ ES7DYP_TER5LV/ES7DYP_TER5LV/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ES7DYP_TER5LV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aknakereso ES7DYP - TER5LV");
+        }
+    }
+}
